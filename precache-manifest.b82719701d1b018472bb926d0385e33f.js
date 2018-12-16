48b4c08cfb405ee8f31b128fef232dd6c8aa6e1f
Fernando Nunes <onlyfernando@gmail.com>
2018-12-16 20:03:50 -0200

    Updates

---
+++ precache-manifest.b82719701d1b018472bb926d0385e33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d111bd30416ad58dd1b2",
+    "url": "/static/js/main.d111bd30.chunk.js"
+  },
+  {
+    "revision": "364ee70045a5f619c45a",
+    "url": "/static/js/1.364ee700.chunk.js"
+  },
+  {
+    "revision": "d111bd30416ad58dd1b2",
+    "url": "/static/css/main.2b1c9a2b.chunk.css"
+  },
+  {
+    "revision": "364ee70045a5f619c45a",
+    "url": "/static/css/1.8bb07371.chunk.css"
+  },
+  {
+    "revision": "6bc3fa11304d14bf3229fac9a7858a82",
+    "url": "/index.html"
+  }
+];
