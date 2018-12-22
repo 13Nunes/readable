70c432ff4ee77624e29db097e177fecb47ec71df
Fernando Nunes <onlyfernando@gmail.com>
2018-12-22 17:29:33 -0200

    botões de ordenação

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -6,12 +6,23 @@ import PostItem from '../../components/PostItem/PostItem';
 import User from '../../components/User/User';
 import Categories from '../../components/Categories/Categories'
 
+// UI
+import { ButtonGroup, Button } from 'reactstrap';
+
 class PostList extends Component {
   render() {
     return (
       <div className="post-list">
         <div className="row">
           <div className="col-sm-8">
+            <div className="toolbar text-right">
+              Sort by&nbsp;
+              <ButtonGroup size="sm">
+                <Button outline color="info">Date</Button>
+                <Button outline color="info">Votes</Button>
+              </ButtonGroup>
+            </div>
+            <br />
             {this.props.posts.map(post => (
               <PostItem post={post} key={post.id} />
             ))}
